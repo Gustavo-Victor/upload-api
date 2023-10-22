9af8936ccb2410cbbb5ec7acc73ccb0dd9c75dee
Gustavo Victor <gguvictor909@gmail.com>
2023-10-22 16:47:52 -0300

    feat: update pictureControllers.js file

---
+++ controllers/pictureControllers.js
@@ -0,0 +1,13 @@
+import { Picture } from "../models/Picture.js";
+
+export async function create(req, res) {
+    res.json("OK")
+}
+
+export async function read(req, res) {
+    res.json("OK"); 
+}
+
+export async function remove(req, res) {
+    res.json("OK"); 
+}
