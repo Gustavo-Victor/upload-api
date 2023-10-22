0735cd154dfc6ed9852a0741cda3364c23cf7d34
Gustavo Victor <gguvictor909@gmail.com>
2023-10-22 16:07:33 -0300

    feat: create server with express

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express"; 
+import { config } from "dotenv"; 
+import mongoose from "mongoose"; 
+config(); 
+
+const app = express(); 
+const port = process.env.PORT; 
+
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json()); 
+app.use(express.static('public')); 
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Hello World!</h1>`);
+});
+
+
+const listener = app.listen(port || 3000, () => {
+    console.log(`App is running on http://localhost:${port}`); 
+});
+
